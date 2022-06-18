e71b2558de5f88e2215dd661683e8fc3a6c9e43c
Muhammad Izzat Farhan <96675193+Isaac9804@users.noreply.github.com>
2022-06-18 22:21:58 +0800

    Delete devel tfmini

--- tfmini_catkin/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/izzat/tfmini_catkin/src
-# Build directory: /home/izzat/tfmini_catkin/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("TFmini-ROS/tfmini_ros")
--- tfmini_catkin/build/TFmini-ROS/tfmini_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/izzat/tfmini_catkin/src/TFmini-ROS/tfmini_ros
-# Build directory: /home/izzat/tfmini_catkin/build/TFmini-ROS/tfmini_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
