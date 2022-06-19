33b2d2f323238614ca1b111be002e58bfb0e7819
isaac9804 <izzat98.work@gmail.com>
2022-06-19 11:46:48 +0800

    Lidar update

---
+++ Lidar_catkin/build/tfmini_ros/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/izzat/Desktop/Lidar_catkin/src/tfmini_ros
+# Build directory: /home/izzat/Desktop/Lidar_catkin/build/tfmini_ros
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
